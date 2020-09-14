9c521e4218433fdda9de6440aa626ab78966c1d6
ron-pixel <68046169+ron-pixel@users.noreply.github.com>
2020-09-14 18:31:42 +0530

    Create PRAC1.py
    python file

---
+++ PRAC1.py
@@ -0,0 +1,14 @@
+import telepot
+import datetime
+
+token='1194605596:AAGUUDPGVtOrMMFdIcx3Esk_gQMotPwmQ7Q'
+bot = telepot.Bot(token)
+print (bot.getMe())
+
+    
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    if content_type == 'text':
+        bot.sendMessage(chat_id, "You said '{}'".format(msg["text"]))
+bot.message_loop(handle)
